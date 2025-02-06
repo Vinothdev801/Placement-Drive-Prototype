ec6258c6483b8cb62650cd72b0d0ce310884412b
AV <avinoth355@gmail.com>
2025-02-06 15:31:54 +0530

    sql file updated.

--- src/main/webapp/SQL/project.sql
+++ src/main/webapp/SQL/project.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Feb 06, 2025 at 10:54 AM
+-- Generation Time: Feb 06, 2025 at 11:00 AM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
@@ -21,64 +21,12 @@ SET time_zone = "+00:00";
 -- Database: `project`
 --
 
--- --------------------------------------------------------
-
---
--- Table structure for table `admin`
---
-
-CREATE TABLE `admin` (
-  `sno` int(11) NOT NULL,
-  `aname` text NOT NULL,
-  `ausername` varchar(25) NOT NULL,
-  `password` varchar(25) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
 --
 -- Dumping data for table `admin`
 --
 
 INSERT INTO `admin` (`sno`, `aname`, `ausername`, `password`) VALUES
 (1, 'Admin1', 'Admin', 'Admin@1234');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `appliedcompany`
---
-
-CREATE TABLE `appliedcompany` (
-  `id` int(11) NOT NULL,
-  `cid` int(11) NOT NULL,
-  `regno` varchar(20) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `appliedcompany`
---
-
-INSERT INTO `appliedcompany` (`id`, `cid`, `regno`) VALUES
-(1, 1, 'RA2332241010424'),
-(2, 5, 'RA2332241010390'),
-(5, 6, 'RA2332241010390'),
-(6, 1, 'RA2332241010390');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `recruitments`
---
-
-CREATE TABLE `recruitments` (
-  `cid` int(25) NOT NULL,
-  `company_name` text NOT NULL,
-  `email` varchar(50) NOT NULL,
-  `role` text NOT NULL,
-  `eligibility` text NOT NULL,
-  `skill` text NOT NULL,
-  `batch` varchar(50) NOT NULL,
-  `package` varchar(20) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `recruitments`
@@ -92,88 +40,12 @@ INSERT INTO `recruitments` (`cid`, `company_name`, `email`, `role`, `eligibility
 (5, 'TCS', 'tcs@gmail.com', 'Game dev', '60 % required', 'C++', '2025', '23 LPA'),
 (6, 'Wipro', 'wipro@gmail.com', 'Associate Software', '65% Required in 10th,12th,UG', 'C,C++,Java', '2025Batch', '7.8LPA');
 
--- --------------------------------------------------------
-
---
--- Table structure for table `student`
---
-
-CREATE TABLE `student` (
-  `regno` varchar(16) NOT NULL,
-  `name` text NOT NULL,
-  `email` varchar(50) NOT NULL,
-  `password` varchar(20) NOT NULL,
-  `X` float NOT NULL,
-  `Xll` float NOT NULL,
-  `ugcgpa` float NOT NULL,
-  `pgcgpa` float NOT NULL,
-  `skills` text NOT NULL,
-  `arrear` text NOT NULL,
-  `course` varchar(30) NOT NULL,
-  `gender` text NOT NULL,
-  `dob` varchar(10) NOT NULL,
-  `photo` varchar(255) DEFAULT NULL,
-  `resume` varchar(255) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
 --
 -- Dumping data for table `student`
 --
 
 INSERT INTO `student` (`regno`, `name`, `email`, `password`, `X`, `Xll`, `ugcgpa`, `pgcgpa`, `skills`, `arrear`, `course`, `gender`, `dob`, `photo`, `resume`) VALUES
-('407', 'santhosh', 'santhos1789@gmail.com', '1234', 80, 80, 7.8, 7.8, 'java, c++,c', 'NO ARREAR', 'MCA', 'MALE', '2002-12-10', 'http://sang.photo', 'http://sangresume'),
-('RA2332241010390', 'Vinoth A', 'testmail@gmail.com', '1234', 83.6, 65, 7.985, 8.78, 'java    ', 'NO ARREAR', 'MCA', 'MALE', '2003-09-16', 'http://avphoto', 'http://avresume'),
-('RA2332241010424', 'Karthik Raja L', 'karthikraja0903@gmail.com', 'Karthik', 82, 65, 80, 8.43, 'Python    ', 'NO ARREAR', 'MCA', 'MALE', '2003-03-16', 'https://karthik/', 'https://karthikresume/');
-
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `admin`
---
-ALTER TABLE `admin`
-  ADD UNIQUE KEY `UNIQUE` (`sno`);
-
---
--- Indexes for table `appliedcompany`
---
-ALTER TABLE `appliedcompany`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `recruitments`
---
-ALTER TABLE `recruitments`
-  ADD PRIMARY KEY (`cid`);
-
---
--- Indexes for table `student`
---
-ALTER TABLE `student`
-  ADD PRIMARY KEY (`regno`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `admin`
---
-ALTER TABLE `admin`
-  MODIFY `sno` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
-
---
--- AUTO_INCREMENT for table `appliedcompany`
---
-ALTER TABLE `appliedcompany`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
-
---
--- AUTO_INCREMENT for table `recruitments`
---
-ALTER TABLE `recruitments`
-  MODIFY `cid` int(25) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+('407', 'santhosh', 'santhos1789@gmail.com', '1234', 80, 80, 7.8, 7.8, 'java, c++,c', 'NO ARREAR', 'MCA', 'MALE', '2002-12-10', 'http://sang.photo', 'http://sangresume');
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
